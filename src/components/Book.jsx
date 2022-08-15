852ddde9191caf5cb03bc7d4e2fb78f950b68940
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-15 18:34:52 +0100

    add redux files

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-
+import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { removeBook } from '../redux/books/books';
 
 function Book(props) {
-  const { title, Author } = props;
+  const dispatch = useDispatch();
+  const { id, title, author } = props;
+
+  const bookRemove = () => {
+    dispatch(removeBook(id));
+  };
   return (
     <div>
       <div className="book">
         <div className="book-info">
           <h2>{title}</h2>
-          <h3>{Author}</h3>
+          <h3>{author}</h3>
         </div>
         <div className="btn-control">
-          <button type="button">Remove</button>
+          <button type="button" onClick={bookRemove}>
+            Remove
+          </button>
         </div>
       </div>
     </div>
@@ -20,7 +27,8 @@ function Book(props) {
 }
 Book.propTypes = {
   title: PropTypes.string.isRequired,
-  Author: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
