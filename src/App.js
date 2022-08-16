ed3d376aa455f38c735faa0b17aa673d88f506ab
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-16 10:53:48 +0100

    add MIT link and install sass for usage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import Nav from './components/Nav';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
